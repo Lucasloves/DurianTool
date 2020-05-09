c730f553be26fa6aab83c5934139d0476cf70dad
Lucasloves <47687210+Lucasloves@users.noreply.github.com>
2020-05-09 19:53:05 +0800

    正式第一次提交

---
+++ src/main/java/com/github/Lucasloves/durian/java/Hello.java
@@ -0,0 +1,9 @@
+package com.github.Lucasloves.durian.java;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/main/java/com/github/Lucasloves/durian/java/User.java
@@ -0,0 +1,9 @@
+package com.github.Lucasloves.durian.java;
+
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+
+}
